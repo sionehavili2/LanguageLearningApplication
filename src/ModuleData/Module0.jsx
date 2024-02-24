7c7c87b58ebfadccec3c76766ec301346d033598
Sione Havili <sionehavili2@gmail.com>
2024-02-23 18:43:51 -0700

    Module0 data has been finished by adding all remaining lessons to that module (6 lessons).

--- src/ModuleData/Module0.jsx
+++ src/ModuleData/Module0.jsx
@@ -7,6 +7,9 @@ const Module0 =
         "Vowels",
         "Dipthong Contrasts",
         "Lengthened Vowel Contrast",
+        "Consonants",
+        "Glottal Stop",
+        "Word Stress",
     ],
 
     lessons :     
@@ -53,7 +56,68 @@ const Module0 =
                ["Contrast the Following", "langa : to build, built, pain", "langā : to be in labor with a child"],
             ]
         },
+
+        {
+            intro : "We will now go over all consonants used in the Tongan language",
+            examples : 
+            [
+              ["f", "f as in father", "fai, fekau"],
+              ["h", "h as in hat", "hiva, hopo"],
+              ["k", "k sound is a mixture between the k sound in 'kettle' and the g sound in 'get'", "ketu, kofu"],
+              ["l", "similar to the l sound made in 'live'. Usually pronounced with a single flip of the tongue", "li, lava, mālie, kelekele"],
+              ["m", "m as in men", "mā, momo"],
+              ["n", "n as in note", "nima, nofo"],
+              ["ng", "ng as in singer (not the same as finger)", "Tonga, ngofua, ngafa"],
+              ["p", "p sound is a mixture between the p sound in 'pad' and the b sound in 'bad'","pehē, pō, pito"],
+              ["s", "s as in sit", "sino, sola"],
+              ["t", "t as in 'tend' with a slight mixture of the letter d sound","taha, tokoni, pito"],
+              ["v","v as in very","vale, velo, hiva"],
+            ]
+        },
+
+        {
+            intro : "The glottal stop (') is a very important useful phoneme but is often difficult for English speakers to recognize and pronounce. However, it is not a new sound. A common glottal stop used by English speakrs is 'nuh uh' or 'uh uh' which is used when expressing disagreement or disapproval. the abrupt (sharp) sound in between is a glottal stop. it may sound as if you were sounding out a letter than came a brief stop. The glottal stop is used very often within the Tongan language and should be practiced throughout your learning experience. Early mastery of the glottal stops will ensure proper pronunciation and ensure learners do not mispronounce or say something with a completely different meaning.",
+            examples : 
+            [
+                ["Distinguish between the following example","'anga : shark", "anga : dispostion"],
+                ["Distinguish between the following example","'ono : barricuda","ono : six"],
+                ["Distinguish between the following example","'uma : kiss","uma : shoulder"],
+                ["Distinguish between the following example","'au : a current in the ocean or any body of water.","au : me"],
+                ["Distinguish between the following example","'omi : bring","omi : come"],
+                ["Distinguish between the following example","'utu : draw (water)","utu : harvest"],
+                ["Medial glottal stops (minimal contrast)", "ma'u : get", "mau : we (exclusive)"],
+                ["Medial glottal stops (minimal contrast)", "va'e : foot", "vae : divide"],
+                ["Medial glottal stops (minimal contrast)", "ta'o : bake", "tao : spear"],
+                ["Medial glottal stops (minimal contrast)", "ta'u : year", "tau : war"],
+                ["Medial glottal stops (minimal contrast)", "to'a : brave", "toa : ironwood tree"],
+                ["Medial glottal stops (minimal contrast)", "to'i : drip", "toi : hide"],
+                ["Medial glottal stops (minimal contrast)", "fu'u : large", "fū : clap (of hands in a kava ceremony)"],
+            ]
+        },
+        
+        {
+            intro : "",
+            examples : 
+            [
+                ["Normally, the stress fall on the next to the last syllable", "fàle : house", "pùha : box", "fanòngo : listen", "fonùa : land, country", "lanumàta : green", "'Ohonùa : a village in Éua", "fakamatàla : to explain, explanation", "faingamālìe : opportunity"],
+                ["However, when a common noun or title is particularized by the definite articles 'e' or 'he' the stress (called the definitive accent) is shifted to the last vowel", "he falè : the house", "e fale lanumatà", "he fonuà : the country" , "e fakamatalà : the explanation" ],
+            ],
+        },
+
     ]
 };
+
+/**** Empty Bracket *** /
+    
+    {
+        intro : "",
+        examples : 
+        [
+            
+        ]
+    },
+
+
+*/
 
 export default Module0;
--- src/UI/StudySession.jsx
+++ src/UI/StudySession.jsx
@@ -1,14 +1,20 @@
 import Flashcards from "./Flashcards";
+import classes from "./StudySession.module.css";
 
 const StudySession = (props) => 
 {
 
     return (
-       <>
-        <h3>{props.moduleData.moduleTitle}</h3>
-        <h4>{props.moduleData.lessons[props.lessonIndex].intro}</h4>
-        <Flashcards exampleArray={props.moduleData.lessons[props.lessonIndex].examples}/>
-       </>
+       <div className={classes.mainContainer}>
+            
+            <>
+                <h2>Flashcard Component</h2>
+                <h3>{props.moduleData.moduleTitle}</h3>
+                <h4>{props.moduleData.lessons[props.lessonIndex].intro}</h4>
+                <Flashcards exampleArray={props.moduleData.lessons[props.lessonIndex].examples}/>
+            </>
+
+       </div>
     );
 }
 
