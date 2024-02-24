8719fe5267c611ab9145fb9933cf06677b446297
Sione Havili <sionehavili2@gmail.com>
2024-02-23 20:40:36 -0700

    Update Module1.jsx
    Module 1 has been added and all lessons to module 1 have been added (3 lessons)

--- src/ModuleData/Module1.jsx
+++ src/ModuleData/Module1.jsx
@@ -1,35 +1,79 @@
 const Module1 =
 {
     
-    moduleTitle : "Greetings and Expressions", 
+    moduleTitle : "Common Greetings, Expressions & Farewells", 
     moduleDescription : "The following is a list of helpful common expressions. It is best to memorize them as soon as possible as they will be used often.",
     lessonTitles : 
     [
         "Greetings", 
         "(Classroom) Expressions",
+        "Farewells",
     ],
 
     lessons :
     [
         {
-            intro : "Remember to concentrate on the meaning as well that the pronunciation.",
+            intro : "Remember to concentrate on the meaning as well that the pronunciation",
             examples : 
             [
+                ["mālō e lelei", "a common greeting or 'hello'"],
+                ["fēfē hake?","How are you?"],
+                ["sai pē!", "just fine!"],
+                ["Ko au", "A polite response when someone calls your name"],
+                ["Mālō e lava mai", "Thanks for coming!"],
+                ["'Io Mālō e tau mo eni", "A proper response to 'Mālō e lava mai'"],
+            ]
 
-            
-            ],
-            outro: "this should be th outro for module 1 lesson 0",
         },
+
         {
-            intro : "this is an empty intro in module 1 lessson 1",
+            intro : "Remember to concentrate on the meaning as well that the pronunciation",
             examples : 
             [
-
+                ["Ko hai ho hingoà?","What is your name?"],
+                ["Ko _____ au.", "I am  _____."],
+                ["Ko e hā e lea faka-Tonga ki he ____ ?", "What is the Tongan word for ____ ?"],
+                ["'Oku 'ikai te u ilo.", "I don't Know."],
+                ["Fanongo pē!", "Just Listen!"],
+                ["'Oua e lea, Fanongo pē!", "Don't speak, just Listen!"],
+                ["Fanongo lelei!", "Listen carefully!"],
+                ["Lea māmālie!", "Speak slowly!"],
+                ["Ko ia!", "Thats right! (correct)"],
+                ["Fakamolemole!", "I am sorry (apologetic)"],
+                ["Kātaki!", "Please excuse (me)!"],
+                ["Kātaki fakamolemole!", "Please be patient (empathetic)!"],
+                ["Tali mai!", "Answer me!"],
+                ["Toe 'ai!", "Repeat!"],
+                ["'Ai fakataha!", "Say it together!"],
+                ["Kātaki toe 'ai!", "Please repeat!"],
+                ["Ki'i tahataha hake!", "A little louder!"],
+                ["Mahino?", "Understand?"],
+                ["'Oku 'ikai mahino!","It is not understandable!"],
+                ["'Oku mahino!","It is understandable!"],
+                ["Mālō 'aupito", "Thank you very much!"],
+                ["Ko e hā ē?", "What is that?"],
+                ["Kamata!", "Begin!"],
+                ["Kamata fo'ou!", "Begin anew!"],
+                ["Sai, tau tuku!", "Fine, lets quit!"],
+                ["Tulou!", "Excuse me! (When you have just passed in front of someone or reached over in front of someone)"],
+                ["Hū mai!", "Come in!"],  
+            ]
+        },        
+        
+        {
+            intro : "In most farewells, there is a rising intonation in the voice on the final 'ē'",
+            examples : 
+            [
+                ["'Alu ā ē!'", "Goodbye! (To the person going)"],
+                ["Nofo ā ē!'", "Goodbye! (To the person staying)"],
+                ["Mou ō ā ē!'", "Goodbye! (To serveral persons going)"],
+                ["Mou nofo ō ā ē!'", "Goodbye! (To serveral persons going)"],
+                ["Mou nofo ō ā ē!'", "Goodbye! (To serveral persons staying)"],
+                ["Faka'au ā ē!", "Goodbye! (Polite form, singular)"],
+                ["Faka'au ā ē!", "Goodbye! (Polite form, plural)"],
             ]
         },
-
     ],
-
 };
 
 export default Module1;
