d95e0ceddc5224109d7e3eac87f3a3157f987895
Sione Havili <sionehavili2@gmail.com>
2024-04-16 19:11:15 -0600

    Memory Mini Game Feature is complete and has responsive css design
    Took almost a full week to figure out memory mini game. found the issue to be a creating compnents before rendering when i should be creating compnent at render with react. Restarted 3x in order to figure that out.

--- src/UI/StudySession/MemoryGame/MemoryGame.jsx
+++ src/UI/StudySession/MemoryGame/MemoryGame.jsx
@@ -2,7 +2,10 @@ import { useState, useEffect } from "react";
 import  shuffle  from "lodash/shuffle";
 import classes from "./MemoryGame.module.css";
 import PlayingCard from "./PlayingCard/PlayingCard.jsx"
+import { conforms } from "lodash";
+import { push, ref } from "firebase/database";
 
+//checks the offset within the moduleData to use the proper index
 function getIndexValues(singleExampleArr)
 {
 
@@ -22,58 +25,133 @@ function getIndexValues(singleExampleArr)
     return [firstIndex, secondIndex];
 }
 
-function getCardDeck(exampleArray) 
+function initializeDeck(exampleArray)
 {
-    console.log(exampleArray)
+    //get proper indexes
     let indexes = getIndexValues(exampleArray[0]);
     let firstIndex = indexes[0];
     let secondIndex = indexes[1];
 
-    let allCards = [];
-    let currIndex = 0;
 
+    let returnDeck = [];
+    let matchIndex = 0;
+    
     exampleArray.forEach(element => 
     {
-        console.log("index: " + currIndex);
-        console.log(element[firstIndex]);
-        console.log(element[secondIndex]);
+        //extract card info
+        let cardInfo = [];
+        cardInfo.push(matchIndex);
+        cardInfo.push(element[firstIndex]);
 
+        //extract matching card info
+        let matchingCardInfo = [];
+        matchingCardInfo.push(matchIndex);
+        matchingCardInfo.push(element[secondIndex]);
 
-        //Push a playing card on
-        allCards.push(<PlayingCard indexValue={currIndex} backText={element[firstIndex]}/>)
-        //Push its matching Valud on as well
-        allCards.push(<PlayingCard indexValue={currIndex} backText={element[secondIndex]}/>)
+        //add both to deck
+        returnDeck.push(cardInfo);
+        returnDeck.push(matchingCardInfo);
 
-        currIndex++;
+        //increment match index
+        matchIndex++;
     });
 
-    return (shuffle([...allCards]))
+    return shuffle(returnDeck);
 }
+
 
 const MemoryGame = (props) => 
 {
-    // const [examples] = useState(shuffle(props.lessonData.examples));
-    const [cardIndex1, setCardIndex1] = useState(null);
-    const [cardIndex2, setCardIndex2] = useState(null);
+    const totalMatches = props.lessonData.examples.length;
+    const [matchCount, setMatchCount] = useState(0);
+    const [deckState ,setDeckState] = useState(Array(props.lessonData.examples.length * 2).fill(false));
+    const [deck, setDeck] = useState(initializeDeck(props.lessonData.examples));
+    const [userSelect, setUserSelect] = useState(null);
+    const [displayFeedback, setDisplayFeedback] = useState(null);
+    const [isFinished, setIsFinished] = useState(false);
 
-    const onSelectCard = (cardIndex) => 
+    function handleSelected(index, matchIndex) 
     {
-        if(cardIndex1 === null)
-            setCardIndex1(cardIndex);
-        else if(cardIndex2 === null)
-            setCardIndex2(cardIndex);
+        //update that card to be flipped
+        deckState[index] = true;
+        setDeckState([...deckState]);
+
+        //if no previous card has been selected, this is the first card
+        if(userSelect === null)
+        {
+            console.log("1st card selected. setting to userselect");
+            setUserSelect([index,matchIndex]);
+        }
+        //else another card has already been selected you are ready to compare
         else
         {
-            console.log("2 cards are already selected...")
+            console.log("2nd card selected READY TO COMPARE");
+
+            let firstIndex = userSelect[0];
+            let firstMatchIndex = userSelect[1];
+            //if match is correct, set cards to null (empty)
+            if(firstMatchIndex === matchIndex)
+            {
+                console.log("CORRECT MATCH!!!");
+
+                deckState[index] = null;
+                deckState[firstIndex] = null;
+                //display userfeedback (true = display will show user was correct)
+                setDisplayFeedback(true);
+                //increment match count
+                setMatchCount(currentCount => currentCount+1);
+            }
+            //else if match is incorrect set cards to false (unflip)
+            else
+            {
+                console.log("incorrect match!....");
+                deckState[index] = false;
+                deckState[firstIndex] = false;
+                //false = display will show user isincorrect
+                setDisplayFeedback(false);
+            }
+            //reset user selected
+            setUserSelect(null);
+            //time to remove display and update cards
+            setTimeout(() => {setDisplayFeedback(null); setDeckState([...deckState]);}, 1000);
         }
-
-    };
-
-    const cardDeck = getCardDeck(props.lessonData.examples, onSelectCard);
+    }
+    
+    useEffect(()=>
+    {
+        if(matchCount === totalMatches)
+        {
+            props.onFinished(true);
+            setTimeout(() => {setIsFinished(true);}, 1100);
+        }
+    },[matchCount])
 
     return (
         <>
-            <div className={classes.mainContainer}>{cardDeck}</div>
+            <h2 className={classes.title}>{props.moduleTitle}</h2>
+            <div className={classes.subTitles}>
+                <h2>{props.lessonTitle} Memory Game</h2>
+                <h4>{"Play a game of Memory! Select a card and its translation"}</h4>
+            </div>
+            <div className={classes.quizContainer}></div>
+
+            <div>Matches {matchCount} / {totalMatches}</div>
+
+            <div className={classes.mainContainer}>
+                {isFinished === true && (
+                    <div className={classes.feedbackContainer}>
+                        <div className={classes.goodFeedback}>
+                                <div>You are Finished!</div>
+                             <button className={classes.returnBtn} onClick={()=>{props.onReturnToCheckPointSelection()}}>Return to checkpoint selection</button>
+                        </div>
+                                           
+                    </div>)}
+                {displayFeedback === true && (<div className={classes.feedbackContainer}><div className={classes.goodFeedback}>Correct Match!</div></div>)}
+                {displayFeedback === false && (<div className={classes.feedbackContainer}><div className={classes.badFeedback}>No Match...</div></div>)}
+        
+                <>{deck.map((cardArray, index) => (<PlayingCard key={index} keyIndex={index} matchIndex={cardArray[0]} displayText={cardArray[1]} isFlipped={deckState[index]} onSelected={(index,matchIndex)=>{handleSelected(index,matchIndex)}} />))}</>
+            </div>
+
         </>
     );
 }
--- src/UI/StudySession/MemoryGame/PlayingCard/PlayingCard.jsx
+++ src/UI/StudySession/MemoryGame/PlayingCard/PlayingCard.jsx
@@ -1,17 +1,26 @@
-import "./PlayingCard.css"
 import { useState, useEffect } from "react";
+import classes from "./PlayingCard.module.css";
+import { join } from "lodash";
 
 const PlayingCard = (props) => {
 
-  const [isFlipped, setIsFlipped] = useState(false);
+  const [isFlipped , setIsFlipped] = useState();
 
+  useEffect(()=>
+  {
+
+    if(props.isFlipped === true) {setIsFlipped(true); console.log("set flip to true..")}
+    else if(props.isFlipped === false){setTimeout(() => {setIsFlipped(false)}, 500); console.log("set flip to false");}
+    else if (props.isFlipped === null){setTimeout(() => {setIsFlipped(null)}, 500); console.log("set flip to empty(null)")}
+  },[props.isFlipped]);
 
   return (
-    <div className={`card ${isFlipped ? 'flipped' : ''}`} onClick={()=>{setIsFlipped(!isFlipped); console.log(props.indexValue)}} value={props.indexValue}>
-      <div className="front"></div>
-      <div className="back">{props.backText ? props.backText : "EMPTY"}</div>
+    <div className={classes.cardSpot}>
+      {isFlipped === null && <div className={classes.noCard}>{props.displayText}</div>}
+      {isFlipped === false && (<div onClick={()=>{props.onSelected(props.keyIndex, props.matchIndex)}} className={classes.cardFront}>{props.matchIndex}</div>)}
+      {isFlipped === true && (<div className={classes.cardBack}>{props.displayText}</div>)}
     </div>
   );
-}
+};
 
 export default PlayingCard;
